3f9a75ea83c42a2a7972c95125ccc079125da05d
TheoMoulin <theo.moulin.sw@gmail.com>
2019-01-24 15:30:50 +0100

    GEMAH - T3.00 - Type eleve emails
    - ADDED: Affichage de type de l'eleve creer ou pour lequel on a creer une decision dans le mail

--- resources/views/emails/scolarites/eleves/create.blade.php
+++ resources/views/emails/scolarites/eleves/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="d-flex flex-column">
         <div class="alert alert-info">
-            <h4 class="alert-heading">Information : Nouvel(le) élève !</h4>
+            <h4 class="alert-heading">Information : Nouvel(le) élève {{ join(" / ",$eleve->types->pluck("libelle")->toArray()) }} !</h4>
             <p>Un(e) nouvel(le) élève vient d'être ajouté(e) sur GEMAH</p>
             <hr>
             <p class="small text-muted mb-0">Ceci est un message automatique, merci de ne pas y répondre</p>
@@ -11,14 +11,21 @@
     </div>
 
     <div class="card">
-        <div class="card-header gemah-bg-primary">Informations sur l'élève</div>
+        <div class="card-header gemah-bg-primary d-flex justify-content-between">
+            Informations sur l'élève
+            <div>
+                @foreach($eleve->types as $type)
+                    <div class="badge badge-success m-0">{{ $type->libelle }}</div>
+                @endforeach
+            </div>
+        </div>
         <div class="card-body">
             <strong>Nom</strong>: {{ $eleve->nom }} <br>
             <strong>Prénom</strong>: {{ $eleve->prenom }} <br>
-            <strong>Date de naissance</strong>
-                {{ \Carbon\Carbon::parse($eleve->date_naissance)->format('d/m/Y') }} <br>
+            <strong>Date de naissance</strong>: {{ \Carbon\Carbon::parse($eleve->date_naissance)->format('d/m/Y') }} <br>
             <strong>Classe</strong>: {{ $eleve->classe }}
         </div>
+
         <div class="card-footer text-center">
             <a href="{{ route('web.scolarites.eleves.show', [$eleve->id]) }}">
                 <button class="btn btn-sm btn-outline-primary">

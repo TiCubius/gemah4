deedd4456e897d11c1c8dc6ac76786eb8f88ef39
Tartiere Kevin <ktartiere@gmail.com>
2019-01-25 15:22:52 +0100

    GEMAH - T3.00 - Message pour création de ticket
    - ADDED: Il est maintenant possible d'ajouter un message lors de la création d'un ticket

--- resources/views/web/scolarites/eleves/tickets/create.blade.php
+++ resources/views/web/scolarites/eleves/tickets/create.blade.php
@@ -10,16 +10,14 @@
 			<form class="mb-3" action="{{ route("web.scolarites.eleves.tickets.store", [$eleve]) }}" method="POST">
 				{{ csrf_field() }}
 
-				<div class="form-group">
-					<label for="libelle">Libellé</label>
-					<input id="libelle" class="form-control" name="libelle" type="text" placeholder="Ex : Appel téléphonique" value="{{ old("libelle") }}" required>
-				</div>
-
+				@component("web._includes.components.input", ["name" => "libelle", "placeholder" => "Ex : Matériel défaillant"])
+					Libellé
+				@endcomponent
 
 				<div class="form-group">
 					<label for="type_ticket_id">Type</label>
 					<select id="type_ticket_id" class="form-control" name="type_ticket_id" required>
-						<option>Sélectionnez un type de ticket</option>
+						<option hidden value="">Sélectionnez un type de ticket</option>
 						@foreach($typesTicket as $type)
 							@if($type->id === old("type_ticket_id"))
 								<option value="{{ $type->id }}" selected>{{ $type->libelle }}</option>
@@ -28,6 +26,12 @@
 							@endif
 						@endforeach
 					</select>
+				</div>
+
+
+				<div class="form-group">
+					<label class="optional" for="message">Message</label>
+					<textarea id="message" name="message" rows="5" class="form-control"></textarea>
 				</div>
 
 				<div class="d-flex justify-content-center">
